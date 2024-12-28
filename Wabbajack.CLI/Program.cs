da3cb6eff88927bb2aeaae766ae5b61aef424c9c
Alex <alexjdickerson1995@gmail.com>
2024-12-27 23:22:48 -0500

    This removes steam support in order to clean up warnings it was throwing. This feature currently does not work and is not in use.

--- Wabbajack.CLI/Program.cs
+++ Wabbajack.CLI/Program.cs
@@ -52,10 +52,6 @@ internal class Program
                 services.AddSingleton<CommandLineBuilder>();
                 services.AddCLIVerbs();
 
- 
-
-                
-                services.AddSingleton<IUserInterventionHandler, UserInterventionHandler>();
             }).Build();
 
         var service = host.Services.GetService<CommandLineBuilder>();
